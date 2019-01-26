6633581616fef441bb732837b95dc92c82495fe0
Ti Kawamoto <tikawamoto@gmail.com>
2019-01-26 09:45:44 -0500

    Add titles to vid popups

--- js/lunarcannon.js
+++ js/lunarcannon.js
@@ -5,18 +5,20 @@ $('.vid-thm img, .vid-element h3').on('click', function(event){
   console.log(expanded);
   if(expanded === 0){
     let selectedVid = vidURL(event.target);
+    let vidTitle = $(event.target).parent().siblings('h3').text();
+    console.log(vidTitle, " vid title");
     var url = location.href;
     if(selectedVid == 0){
-      popupFadeIn();
+      popupFadeIn(vidTitle);
       $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/pmiduYrGXiU" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     } else if(selectedVid == 1){
-      popupFadeIn();
+      popupFadeIn(vidTitle);
       $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/yo6PT3rLDOQ" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     } else if(selectedVid == 2){
-      popupFadeIn();
+      popupFadeIn(vidTitle);
       $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/ljMeCBNpreg" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     } else if(selectedVid == 3){
-      popupFadeIn();
+      popupFadeIn(vidTitle);
       $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/LTCvCgB8P8k" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     }
     // $(".vid-expand").animate({height:"100%", opacity:"1"},250);
@@ -34,9 +36,10 @@ $('.vid-thm img, .vid-element h3').on('click', function(event){
   });
 });
 
-function popupFadeIn(){
+function popupFadeIn(vidTitle){
   $('#popup-bg').fadeIn(300);
   $('.vid-popup').slideDown(300);
+  $('.close-vid').append(`<h3 class="vid-popup-title">${vidTitle}</h3>`).hide().fadeIn(500);
 }
 
 function closeVid(){
@@ -49,11 +52,10 @@ function closeVid(){
 
 function closePopup(){
   $('#popup-bg').fadeOut(300);
+  $('.vid-popup-title').fadeOut(300);
   $(".vid-popup").slideUp(300, function(){$('.vid-embed iframe').remove();});
   expanded = 0;
 }
-
-
 
 function vidURL(click){
   let vidLink = $(click).attr("alt");
