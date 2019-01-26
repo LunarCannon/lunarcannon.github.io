ee8be69e0f87aa4abd37de559233d2d2b06308cb
Ti Kawamoto <tikawamoto@gmail.com>
2019-01-26 01:21:32 -0500

    popup js finished

--- js/lunarcannon.js
+++ js/lunarcannon.js
@@ -1,36 +1,59 @@
 let expanded = 0;
-$('.vid-thm img').mouseover().css("cursor","pointer");
-$('.vid-thm img').on('click', function(event){
+$('.vid-thm img, .vid-element h3').mouseover().css("cursor","pointer");
+$('.vid-thm img, .vid-element h3').on('click', function(event){
   // $(this).parent().children("div, p").animate({height: "toggle"}, 250);
+  console.log(expanded);
   if(expanded === 0){
     let selectedVid = vidURL(event.target);
     var url = location.href;
     if(selectedVid == 0){
-      $(".vid-expand").append(`<div class="spacer-20"></div><div class="close-vid"><i class="fas fa-window-close close-icon"></i></div><div class="vid-popup"><div class="vid-embed">
-        <iframe src="https://www.youtube.com/embed/pmiduYrGXiU" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div></div>`);
+      popupFadeIn();
+      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/pmiduYrGXiU" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    } else if(selectedVid == 1){
+      popupFadeIn();
+      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/yo6PT3rLDOQ" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    } else if(selectedVid == 2){
+      popupFadeIn();
+      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/ljMeCBNpreg" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    } else if(selectedVid == 3){
+      popupFadeIn();
+      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/LTCvCgB8P8k" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     }
-    $(".vid-expand").animate({height:"100%", opacity:"1"},250);
-    location.href = "#vidplayer";
-    history.replaceState(null,null,url);
+    // $(".vid-expand").animate({height:"100%", opacity:"1"},250);
+    // location.href = "#vidplayer";
+    // history.replaceState(null,null,url);
     expanded = 1;
     closeVid();
   } else {
-    $(".vid-expand").animate({height:"0px"},300).empty();
-    expanded = 0;
+    closePopup();
   }
-  $('.vid-expand i').mouseover().css("cursor","pointer");
+  $('.close-icon').mouseover().css("cursor","pointer");
+
+  $('#popup-bg').on('click',function(){
+    closePopup();
+  });
 });
+
+function popupFadeIn(){
+  $('#popup-bg').fadeIn(300);
+  $('.vid-popup').slideDown(300);
+}
 
 function closeVid(){
   $(".close-icon").on("click", function(e){
     if(expanded == 1){
-      console.log("close");
-      $(".vid-expand").animate({height:"0px"},300).empty();
-      expanded = 0;
+      closePopup();
     }
   });
 }
+
+function closePopup(){
+  $('#popup-bg').fadeOut(300);
+  $(".vid-popup").slideUp(300, function(){$('.vid-embed iframe').remove();});
+  expanded = 0;
+}
+
+
 
 function vidURL(click){
   let vidLink = $(click).attr("alt");
