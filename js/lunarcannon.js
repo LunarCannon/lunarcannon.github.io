2d94d911614af922074af3059b1e37e6a904ab03
Ti Kawamoto <tikawamoto@gmail.com>
2019-01-26 10:46:44 -0500

    get vid title from clicking on heading

--- js/lunarcannon.js
+++ js/lunarcannon.js
@@ -1,4 +1,10 @@
 let expanded = 0;
+
+$('.lc-circle-logo').animate({opacity:1, top: "-=35px"}, 800, "easeInOutQuint");
+$('.text-box-fade').animate({opacity:1}, 800, "easeInOutQuint");
+$('.fade-text').delay(50).animate({opacity:1, top: "-=10px"}, 500, "easeInOutQuint");
+$('.logo-roll').delay(600).animate({opacity:1, top: "-=10px"}, 800, "easeInOutQuint");
+
 $('.vid-thm img, .vid-element h3').mouseover().css("cursor","pointer");
 $('.vid-thm img, .vid-element h3').on('click', function(event){
   // $(this).parent().children("div, p").animate({height: "toggle"}, 250);
@@ -6,20 +12,24 @@ $('.vid-thm img, .vid-element h3').on('click', function(event){
   if(expanded === 0){
     let selectedVid = vidURL(event.target);
     let vidTitle = $(event.target).parent().siblings('h3').text();
-    console.log(vidTitle, " vid title");
+    if(vidTitle == ''){
+      vidTitle = $(event.target).text();
+      console.log(vidTitle, " vid title");
+    }
+
     var url = location.href;
     if(selectedVid == 0){
       popupFadeIn(vidTitle);
-      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/pmiduYrGXiU" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/pmiduYrGXiU?autoplay=1" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     } else if(selectedVid == 1){
       popupFadeIn(vidTitle);
-      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/yo6PT3rLDOQ" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/yo6PT3rLDOQ?autoplay=1" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     } else if(selectedVid == 2){
       popupFadeIn(vidTitle);
-      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/ljMeCBNpreg" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/ljMeCBNpreg?autoplay=1" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     } else if(selectedVid == 3){
       popupFadeIn(vidTitle);
-      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/LTCvCgB8P8k" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+      $('.vid-embed').append('<iframe src="https://www.youtube.com/embed/LTCvCgB8P8k?autoplay=1" style="position:absolute;top:0;left:0;width:100%;height:100%;opacity=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     }
     // $(".vid-expand").animate({height:"100%", opacity:"1"},250);
     // location.href = "#vidplayer";
@@ -38,7 +48,7 @@ $('.vid-thm img, .vid-element h3').on('click', function(event){
 
 function popupFadeIn(vidTitle){
   $('#popup-bg').fadeIn(300);
-  $('.vid-popup').slideDown(300);
+  $('.vid-popup').slideDown(500, "easeInOutCirc");
   $('.close-vid').append(`<h3 class="vid-popup-title">${vidTitle}</h3>`).hide().fadeIn(500);
 }
 
@@ -53,7 +63,7 @@ function closeVid(){
 function closePopup(){
   $('#popup-bg').fadeOut(300);
   $('.vid-popup-title').fadeOut(300);
-  $(".vid-popup").slideUp(300, function(){$('.vid-embed iframe').remove();});
+  $(".vid-popup").slideUp(500, "easeInOutCirc", function(){$('.vid-embed iframe').remove();});
   expanded = 0;
 }
 
